616848797ff18c358c6900d3668cb4b07ca72b85
gotme_Krackin <jeremyjosh.abad.21@usjr.edu.ph>
2024-03-11 23:36:17 +0800

    Implementing Movement
    basic movement

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    Rigidbody2D player_rigidbody2D;
+    float walk_speed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player_rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Walking Right");
+            transform.position += Vector3.right * walk_speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Walking Left");
+            transform.position += Vector3.left * walk_speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Jumping");
+            //transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Crouching");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        player_rigidbody2D.velocity = new Vector2(0, player_rigidbody2D.velocity.y);
     }
 }
