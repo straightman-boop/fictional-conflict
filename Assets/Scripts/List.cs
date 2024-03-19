c64ca42f259ff3709b74e6676bb69951d7a31502
gotme_Krackin <jeremyjosh.abad.21@usjr.edu.ph>
2024-03-19 18:28:03 +0800

    Listed requirements

---
+++ Assets/Scripts/List.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class List : MonoBehaviour
+{
+    /*
+    
+    Current Scripts:
+
+    Movement Script
+    DoublePress Script
+    Grounded Script
+    Dash Script
+    Running Script
+    Hurt Script
+    
+    
+    Projected Requirements:
+
+    Arts:
+        Character Sprites
+            - Pumpkin_Head
+            - ??
+        Action Sprites and Animations
+            - Idle
+            - Light Attack, Medium Attack, Heavy Attack, and Combos
+            - Running
+            - Aerial Dash
+            - Blocking (Standing and Low)
+            - Specials 
+            - Defeated
+    
+    Programming:
+        Scene Management
+        Health System
+        UI System
+        Combat System 
+            - (Light, Medium, Heavy, and Combos)
+            - (Quarter Circles and Half Circles) For Ranged And Specials
+            - Parries (Maybe)
+        Sprite Flip System
+    
+    UI/UX:
+        Main Menu
+        Character Selection
+        Battle Scene
+
+    SFX:
+        UI SFX
+        Character SFX
+        Attack SFX
+        Battle Scene SFX
+    
+    
+    
+    */
+}
